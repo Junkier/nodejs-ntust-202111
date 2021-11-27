ed75112690e29db6e5154b215faf61ac842cd198
Jeff.hsuan <logFM3798@gmail.com>
2021-11-27 13:14:36 +0800

    Updating

--- 2021_1127/server.js
+++ 2021_1127/server.js
@@ -8,16 +8,22 @@ app.get("/" , (req,res)=>{
 // app.get("/testqq")  --> 路徑 , end-point 
 // (req,res)=>{ ... }  --> 處理函式
 // res.send            --> response 的回覆處理
-
 app.get("/testqq" ,(req,res)=>{
     res.send("嗨嗨 , 我是 /testqq 路徑");
 });
 
+app.get("/this-is-a-book" , (req,res)=>{
+    console.log("今天星期六！");
+    console.log("好冷好冷～");
+    res.send("中午吃啥麼！？");
+});
+
+
 // 讓 server.js 佔有 8088 的 port
 app.listen(8088,()=>{
     console.log("Server is running at localhost:8088");
-    console.log("嗨嗨 , 我是第一次看 Node.js 程式");
-    console.log("怎麼有點難ＱＱ");
+    // console.log("嗨嗨 , 我是第一次看 Node.js 程式");
+    // console.log("怎麼有點難ＱＱ");
 });
 
 // 程式執行後 , 進入 "等待" (Pending) 的狀態 
