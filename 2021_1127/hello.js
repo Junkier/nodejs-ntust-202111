075ed3140bd1bd8a9b846ee8f730c5dca059adb3
Jeff.hsuan <logFM3798@gmail.com>
2021-11-27 11:37:37 +0800

    Updating

--- 2021_1127/hello.js
+++ 2021_1127/hello.js
@@ -1,1 +1,10 @@
-console.log("Hello World !!!");
+// console.log("Hello World !!!");
+
+////////////////////////////////////////////////////////////
+// Function (函式)
+// 兩數相加的函式
+let add = (a,b) => a+b;
+
+// console.log --> 將結果顯示到畫面上
+console.log( add(1,2) );
+console.log( add(-1,17) );
--- 2021_1127/server.js
+++ 2021_1127/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 
-
 app.get("/" , (req,res)=>{
    res.send("嗨嗨 , 我是 Node.js server !!!");
+});
+
+// app.get("/testqq")  --> 路徑 , end-point 
+// (req,res)=>{ ... }  --> 處理函式
+// res.send            --> response 的回覆處理
+
+app.get("/testqq" ,(req,res)=>{
+    res.send("嗨嗨 , 我是 /testqq 路徑");
 });
 
 app.listen(8088,()=>{
     console.log("Server is running at localhost:8088");
 });
+
+
+
+////////////////////////
+// Ctrl+c --> terminal 終止程式
