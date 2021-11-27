245d2bcb5cab0dc0030819bfb58bf359272cd530
Jeff.hsuan <logFM3798@gmail.com>
2021-11-27 16:59:53 +0800

    Updating

--- 2021_1127/tutorial.js
+++ 2021_1127/tutorial.js
@@ -9,7 +9,33 @@ console.log(a-b);
 console.log(a*b);
 console.log(a/b);
 
+console.log( "-".repeat(50) );
+
 // String , 字串 ,  文字資料
 let name2 = "Jeff";
 let message2 = '要下課了啦啦啦～';
 console.log(name2,message2);
+console.log(name2 + "說: " + message2 + "!!!");
+
+console.log( "-".repeat(50) );
+
+// Boolean , 布林值 , 只有 true / false 
+let d = true;
+let e = false;
+console.log("d :",d);
+console.log("e :",e);
+console.log("d && e :", d && e );  // 取交集 , and
+console.log("d || e :", d || e );  // 取連集 , or 
+
+let f = (a > 100) ;  // 請問 a>100 嗎 ?  V-> true ; X -> false
+console.log("f :",f);
+
+console.log( "-".repeat(50) );
+
+
+// Date , 時間資料
+let time = new Date();
+console.log(time);
+console.log( time.getFullYear() );
+console.log( time.getMonth() );
+console.log( time.getMinutes() );
