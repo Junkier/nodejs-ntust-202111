283283d16c6477194186cd2abb147c063ba8f5a0
Jeff.hsuan <logFM3798@gmail.com>
2021-12-04 13:37:39 +0800

    Updating

--- 2021_1204/tutorial.afternoon.js
+++ 2021_1204/tutorial.afternoon.js
@@ -37,3 +37,41 @@ let sayHello = (name) => {
 sayHello("Jeff!!!");
 
 sayHello("Leo ~~~");
+
+console.log("-".repeat(50));
+
+//// function 特性介紹
+let sayHello2 = (name2,age2) => {
+  // name2 , age2 為 input (輸入參數)
+  console.log("[sayHello2] 嗨嗨 , 我是" + name2 + "," + "年紀是" + age2);
+}
+
+let sayHello2_with_output = (name2,age2)=>{
+  // name2 , age2 為 input (輸入參數)
+  console.log("[sayHello2_with_output] 嗨嗨 , 我是" + name2 + "," + "年紀是" + age2);
+  
+  // return 後的東西為 output (輸出結果)
+  return "下課囉！！！！";
+};
+
+// sayHello2("Jeff",18);
+// sayHello2_with_output("Jeff",18);
+
+let msg1 = sayHello2("Jeff",18);
+console.log(msg1);
+let msg2 = sayHello2_with_output("Jeff",18);
+console.log(msg2);
+
+
+let arr3 = [1,2,3,4,5];
+// arr3.map(n => n*3) 等同於
+let map3 = arr3.map((n) => {
+  console.log(n);
+  console.log("AAA");
+  console.log("BBB");
+  return n*3;
+});
+
+// let map3 = arr3.map( n => n*3 );
+
+console.log("map3 :" , map3);
