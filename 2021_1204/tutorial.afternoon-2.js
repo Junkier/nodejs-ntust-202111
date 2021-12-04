5780c85cd1e46ef5a000b095e4f68a2fe252ecf8
Jeff.hsuan <logFM3798@gmail.com>
2021-12-04 16:38:08 +0800

    Updating

--- 2021_1204/tutorial.afternoon-2.js
+++ 2021_1204/tutorial.afternoon-2.js
@@ -1,29 +1,74 @@
 //// 流程控制#2 - 迴圈
-let arr1 = [97,100,85,77,89,77,65];
+// let arr1 = [97,100,85,77,89,77,65];
 // let sum1 = arr1[0] + arr1[1] + arr1[2] + ... ; --> 不佳
 
 // for-loop 
 console.log("Start !!!");
 
-// for(let i = 0 ; i < 10 ; i++){
-//   console.log(i);
-// };
+for(let i = 0 ; i < 10 ; i++){
+  console.log(i);
+};
 
-// for(let j=0 ; j<6 ; j = j + 1){
-//   console.log("第" + j + "次執行");
-// };
+for(let j=0 ; j<6 ; j = j + 1){
+  console.log("第" + j + "次執行");
+};
 
-// for(let k = 11 ; k>=0 ; k = k-2){
-//   console.log("嗨嗨嗨嗨嗨");
-//   console.log(k);
-//   console.log("----------");
-// }
+for(let k = 11 ; k>=0 ; k = k-2){
+  console.log("嗨嗨嗨嗨嗨");
+  console.log(k);
+  console.log("----------");
+}
+console.log("End !!!");
 
-for(let i=0 ; i <=6 ; i++){
+let arr1 = [97,100,85,77,89,77,65,77,33,47,56];
+let sum1 = 0;
+
+// for(let i=0 ; i <=6 ; i++){  結束條件寫 i<=6 不佳
+   
+for(let i=0 ; i <= arr1.length-1 ; i++){ // 結束條件寫 i<=arr1.length-1 好！
   // console.log(arr1[0]);
   // console.log(arr1[1]);
   // console.log(arr1[2]);
-  console.log(arr1[i]);
+  // console.log(arr1[i]);
+
+  sum1 = sum1 + arr1[i];
+  // i=0 -> sum1 = (0)  + 97  = 97;
+  // i=1 -> sum1 = (97) + 100 = 97+100;
+  // i=2 -> sum1 = (97+100) + 85 = 97+100+85;
+  // i=3 -> sum1 = (97+100+85) + 77 = 97+100+85+77;
+  // ... 
+  // i=arr1.length-1 算完結束
+
 };
 
-console.log("End !!!");
+console.log("arr1 總和 :" , sum1);
+console.log("arr1 平均 :" , sum1 / arr1.length );
+
+// 1. 計算 1+2+3+…+100 之總和
+
+
+// 2. 試撰寫一程式 , 列印出以下圖形  (repeat)
+//     ******
+//     *****
+//     ****
+//     ***
+//     **
+//     *
+// console.log("*".repeat(6));
+// console.log("*".repeat(5));
+// console.log("*".repeat(4));
+// console.log("*".repeat(3));
+// console.log("*".repeat(2));
+// console.log("*".repeat(1));
+
+// 3.  有一變數 teacherName="Jack!" , 試撰寫一程式 , 列印出下列訊息
+//     JJJJJ
+//     aaaa
+//     ccc
+//     kk
+//     !
+// let teacherName = "Jack!";
+// console.log(teacherName[0]);
+// console.log(teacherName[1]);
+// console.log(teacherName[2]);
+   
