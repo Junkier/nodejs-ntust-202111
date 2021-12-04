85f6f75f1b427a6226016b30d06a6a2e17c686e0
Jeff.hsuan <logFM3798@gmail.com>
2021-12-04 09:12:33 +0800

    Updating

--- 2021_1204/tutorial.morning.js
+++ 2021_1204/tutorial.morning.js
@@ -1,5 +1,31 @@
 //// 資料型別 #1
 // Number  , 數值資料
+console.log(123);
+console.log(3.1415);
+
 // String , 字串 ,  文字資料
-// Boolean , 布林值 , 只有 true / false 
+console.log("傑夫老師");
+console.log('今天禮拜六～');
+console.log(`我是字串～`);  // 數字1 的左邊 
+
+// Boolean , 布林值 , 只有 true / false
+console.log(true);
+console.log(false);
+console.log(123 > 456); // false
+
 // Date , 時間資料
+console.log(new Date());
+let time = new Date();
+console.log(time.getFullYear());
+
+console.log("-".repeat(50));
+
+
+// Array , 陣列資料
+let arr1 = [1,2,3,4,5];
+console.log(arr1);
+
+// Object , 物件資料
+let obj1 = { "name" : "jeff" , "age" : 18 };
+console.log(obj1);
+
