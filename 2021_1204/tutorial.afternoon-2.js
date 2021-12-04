ed0e30cdc376cbd32c4076be23bdc496c9999f2f
Jeff.hsuan <logFM3798@gmail.com>
2021-12-04 16:13:04 +0800

    Updating

---
+++ 2021_1204/tutorial.afternoon-2.js
@@ -0,0 +1,29 @@
+//// 流程控制#2 - 迴圈
+let arr1 = [97,100,85,77,89,77,65];
+// let sum1 = arr1[0] + arr1[1] + arr1[2] + ... ; --> 不佳
+
+// for-loop 
+console.log("Start !!!");
+
+// for(let i = 0 ; i < 10 ; i++){
+//   console.log(i);
+// };
+
+// for(let j=0 ; j<6 ; j = j + 1){
+//   console.log("第" + j + "次執行");
+// };
+
+// for(let k = 11 ; k>=0 ; k = k-2){
+//   console.log("嗨嗨嗨嗨嗨");
+//   console.log(k);
+//   console.log("----------");
+// }
+
+for(let i=0 ; i <=6 ; i++){
+  // console.log(arr1[0]);
+  // console.log(arr1[1]);
+  // console.log(arr1[2]);
+  console.log(arr1[i]);
+};
+
+console.log("End !!!");
