1dba8aa82a3b080bdfd426a6eb529cdee664784c
Jeff.hsuan <logFM3798@gmail.com>
2021-12-18 10:43:29 +0800

    Updating

--- 2021_1218/server.js
+++ 2021_1218/server.js
@@ -4,19 +4,22 @@ const hbs = require("hbs");   // 記得 npm install hbs
 const app = express();
 const portNum = 8088;
 
-// [1] 設定模板引擎
+// [1] 設定模板引擎 (解析 html 檔 , 讓 express 看懂 html 程式)
+// hbs -> handlebars 為一種模版引擎
+// 另外一種熱門的模版引擎 --> pug 
 app.engine("html" , hbs.__express);
 
 // [2] 設定模板 (template) 位置
 app.set("views" , path.join(__dirname , "application" , "views" ));
 
-// [3] 設定靜態黨的位置 
-app.use( express.static(path.join( __dirname , "application")));
+// [3] 設定靜態檔的位置 (讀取 *.css / *.js / *.jpg / *.png / *.mp4 / ...)
+// --> 處理 靜態檔 相關 requests
+app.use( express.static( path.join( __dirname , "application") ));
 
 app.get("/" , (req,res)=>{
   // res.send("嗨嗨,  我是 Node.js server.");
   
-  // [4] 使用 .render 回傳 html 頁面
+  // [4] 使用 .render (渲染) 回傳 html 頁面
   res.render("index.html");
 });
 
