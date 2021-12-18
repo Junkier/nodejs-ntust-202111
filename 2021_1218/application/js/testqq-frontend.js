20b09b9234b2c938b17106077f33747240d8c5b4
Jeff.hsuan <logFM3798@gmail.com>
2021-12-18 11:55:30 +0800

    Done.

--- 2021_1218/application/js/testqq-frontend.js
+++ 2021_1218/application/js/testqq-frontend.js
@@ -1,1 +1,22 @@
-alert("嗨嗨 , 我是 .js 檔 ~~~~");  // 跳出警示訊息
+// alert("嗨嗨 , 我是 .js 檔 ~~~~");  // 跳出警示訊息
+
+
+// 等 HTML 上的標籤完成 , 才開始執行
+// jQuery (Javascript 語法糖)
+// 進階 -> Vue.js / React.js / Angular.js 
+$(function(){
+  console.log("嗨嗨 , 我是 .js ~");
+
+  // 透過 id 修改文字
+  // $("#wording").text(" JS 修改文字～～～");
+
+  setTimeout(()=>{
+    $("#wording").text("JS 修改文字～～～");
+  },1000);
+
+  // 透過 class 綁定 click 事件 （事件聆聽)
+  $(".test-btn").click(()=>{
+    alert("按到按鈕！！！");
+  }); 
+
+});
