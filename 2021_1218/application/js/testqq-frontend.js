5761e2d685054641143fae3e05bcb6684f8e1eb9
Jeff.hsuan <logFM3798@gmail.com>
2021-12-18 11:57:43 +0800

    Done.

--- 2021_1218/application/js/testqq-frontend.js
+++ 2021_1218/application/js/testqq-frontend.js
@@ -1,15 +1,16 @@
 // alert("嗨嗨 , 我是 .js 檔 ~~~~");  // 跳出警示訊息
 
-
-// 等 HTML 上的標籤完成 , 才開始執行
+//////
 // jQuery (Javascript 語法糖)
 // 進階 -> Vue.js / React.js / Angular.js 
-$(function(){
+//////
+
+$(function(){   // 等 HTML 上的標籤完成 , 才開始執行 
   console.log("嗨嗨 , 我是 .js ~");
 
+  // [前端 jQuery 語法]
   // 透過 id 修改文字
   // $("#wording").text(" JS 修改文字～～～");
-
   setTimeout(()=>{
     $("#wording").text("JS 修改文字～～～");
   },1000);
