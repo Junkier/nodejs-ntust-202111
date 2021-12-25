cb768f5d9eb997afef4a0a06893a72779571e177
Jeff.hsuan <logFM3798@gmail.com>
2021-12-25 11:24:04 +0800

    Updating

--- 2021_1225/server.js
+++ 2021_1225/server.js
@@ -49,34 +49,57 @@ app.get("/about/us",(req,res)=>{
 });
 
 
-
+////////////////////////////////////////
+// Middleware (中介函式)
 app.get("/hello" , 
   (req,res,next)=>{
-    // 往下一個 Middleware (中介函式) 執行
+    
     console.log("我是 Middleware 1");
 
-    // 顯示 name 參數
-    console.log(`您是 :${ req.query.name }`);
-    next();
+    // [1] 顯示前端 name 參數
+    // console.log(`您是 :${ req.query.name }`);
+
+    // [2] Middleware 間傳參數
+    // req (request 物件) 上 設定資料
+    req.test = { name : "jeff" , age :18 };
+
+    // [3] 往下一個 Middleware (中介函式) 執行
+    // next();
+
+    // [4] 檢查 name 參數 是否存在
+    // V -> ok , 往下一個 Middleware 執行
+    // X -> 回傳 { message : "name 人呢？" }
+    // 使用 error first 寫法
+    // if( req.query.name === undefined){ // 還可以更好
+    if(!req.query.name){  // 更佳 !!!
+      res.json({ message : "name 人呢？" });
+    }else{
+      next();
+    };
+
   },
   (req,res,next)=>{
+    // 100% 確保 name 參數必存在
     console.log("我是 Middleware 2");
 
-    // 顯示 age 參數
+    // [1] 顯示前端 age 參數
+    // [1] 顯示前端 name 參數
+    console.log(`您是 : ${ req.query.name }`);
     console.log(`您今年 : ${req.query.age} 歲`);
     next();
   },
   (req,res,next)=>{
     console.log("我是 Middleware 3");
+    console.log("req.test :" , req.test);
     next();
   },
   (req,res)=>{
     console.log("我是 Middleware 4");
-    res.send("Hello , 過敏好可怕～～～");
+    res.json({ result : req.test });
+    // res.send("Hello , 過敏好可怕～～～");
   }
 );
-
-
+////////////////////////////////////////
 
 
 
