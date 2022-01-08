f66b7b0f5ea91e42011abea65161e563a492f44c
Jeff.hsuan <logFM3798@gmail.com>
2022-01-08 14:47:54 +0800

    Updating

--- 2022_0108/mongo-tutorial1.js
+++ 2022_0108/mongo-tutorial1.js
@@ -8,7 +8,31 @@ db.getCollection('mongo-tutorial').insertMany([{
     "category" : "犯罪",
     "name" : "絕命毒師",
     "score" : 10.0
-},... ]);
+}, ... ]);
+// 總共有 7 筆
 
-// .find 查看資料
+////////////////////////////////////////////////////////////
+
+// .insert 新增一筆資料
+db.getCollection('mongo-tutorial').insert( { "today" : "星期六" , "weather" : "sunny"} )
+
+// .find 查看 全部資料
 db.getCollection('mongo-tutorial').find({})
+
+// 有條件的 .find(condition)
+// ex1: category="政治"
+db.getCollection('mongo-tutorial').find({ "category" : "政治"})
+
+// ex2: category="政治" && score=9.5
+db.getCollection('mongo-tutorial').find({ "category" : "政治" , "score" : 9.5 })
+
+
+// ex3: category="政治" && score > 9.5
+db.getCollection('mongo-tutorial').find({  
+  "category" : "政治" ,
+  "score" : {
+    "$gt" : 9.5      // > 9.5
+    // "$gte" : 9.5  // >= 9.5 
+  }
+})
+  
