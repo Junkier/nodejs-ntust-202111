178f815bdb8c5d759c0aff711aad50eca81819d2
Jeff.hsuan <logFM3798@gmail.com>
2022-01-08 15:12:46 +0800

    Updating

--- 2022_0108/mongo-tutorial1.js
+++ 2022_0108/mongo-tutorial1.js
@@ -21,11 +21,10 @@ db.getCollection('mongo-tutorial').find({})
 
 // 有條件的 .find(condition)
 // ex1: category="政治"
-db.getCollection('mongo-tutorial').find({ "category" : "政治"})
+db.getCollection('mongo-tutorial').find({ "category" : "政治" })
 
 // ex2: category="政治" && score=9.5
 db.getCollection('mongo-tutorial').find({ "category" : "政治" , "score" : 9.5 })
-
 
 // ex3: category="政治" && score > 9.5
 db.getCollection('mongo-tutorial').find({  
@@ -35,4 +34,45 @@ db.getCollection('mongo-tutorial').find({
     // "$gte" : 9.5  // >= 9.5 
   }
 })
+
+// ex4: score <= 9.5
+db.getCollection('mongo-tutorial').find({  
+    "score" : {  
+         //"$lt" : 9.5  
+         "$lte" : 9.5   
+    }    
+})
+
+// ex5: 找到 age 欄位存在的資料
+db.getCollection('mongo-tutorial').find({  
+    "age" : { 
+        "$exists" :  true
+     }
+})
+
+// ex6: today 欄位'不存在' && category=政治 && score >=9.4 
+db.getCollection('mongo-tutorial').find({  
+     "today" : { 
+        "$exists" :  false
+     },
+     "category" : "政治",
+     "score" : {
+        "$gte" : 9.4    
+     }
+})
   
+// ex7: 搜尋全部 + 顯示 {category , name} 欄位 , _id 不顯示
+// ==>  .find(條件 , 欄位顯示)
+db.getCollection('mongo-tutorial').find( {} ,  { "category" : 1 , "name" : 1 , "_id" : 0 }  )
+
+////////////////////////////////////////////////////////////
+
+// {
+//   // "_id" -> primary_key (主鍵) , 可由 MongoDB 自動產生 ,
+//   // 也可以自行建立
+//     "_id" : ObjectId("61d92d5fdb5049d56e3bbdeb"),   
+//     "dramaId" : "1002",
+//     "category" : "殭屍",
+//     "name" : "屍戰朝鮮",
+//     "score" : 9.0
+// }
