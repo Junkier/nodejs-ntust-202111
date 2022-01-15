d8c60bc3712010dfb14a95ae230742ff9b6389d6
Jeff.hsuan <logFM3798@gmail.com>
2022-01-15 17:14:41 +0800

    Updating

--- sample-4-answer/models/db.js
+++ sample-4-answer/models/db.js
@@ -8,9 +8,9 @@ const logger = require("log4js").getLogger("DB");
 
 
 const conn = mongoose.createConnection(connConfig,{ 
-    maxPoolSize: 5 , 
-    useNewUrlParser: true ,
-    useUnifiedTopology: true,
+    maxPoolSize: 5 ,   // 最多同時建立 5 個連線
+    useNewUrlParser: true ,   // mongoose 新加參數 , 使用 new parser 解析 connConfig 
+    useUnifiedTopology: true, // 串接成 cluster 時 , 使用 new discover & topology 技術
 });
 
 
